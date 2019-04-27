1939d0ddc013c684e8a24d7d7a2c50fb0ced280c
markwreinke <46796939+markwreinke@users.noreply.github.com>
2019-04-27 10:34:28 -0400

    add tostring and scalar mult methods

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+import MissingMatrixInputError
+
 
 class Matrix():
     def __init__(self, numRows, numColumns, *inputs):
@@ -6,19 +8,47 @@ class Matrix():
         if(numRows*numColumns != len(inputs)):
             raise MissingMatrixInputError("There was an incorrect number of inputs.")
 
+        for input in inputs:
+            if not isinstance(input, int) and not isinstance(input, float):
+                raise TypeError()
+
         self.numRows = numRows
         self.numColumns = numColumns
         self.matrixElements = []
 
         # Creates a temporary variable inputs to store the inputs from the arguments
-        inputs = []
-        for inputs in inputs:
-            inputs.append(input)
-        inputs.reverse()
+        functionInputs = []
+        for input in inputs:
+            functionInputs.append(input)
+        functionInputs.reverse()
 
         # organizes the inputs into the matrix elements
         for n in range(0, numRows):
             newRow = []
             for m in range(0, numColumns):
-                newRow.append(inputs.pop())
-            matrixElements.append(newRow)
+                newRow.append(functionInputs.pop())
+            self.matrixElements.append(newRow)
+
+    def to_string(self):
+        stringRepresentation  = ""
+        for n in range(0, self.numRows):
+            stringRepresentation = stringRepresentation + "[ "
+            for m in range(0, self.numColumns):
+                stringRepresentation = stringRepresentation + str(self.matrixElements[n][m]) + " "
+            stringRepresentation = stringRepresentation + "]" + "\n"
+
+        return stringRepresentation;
+
+
+    def scalar_mult(self, alpha):
+        if not isinstance(alpha, int) and not isinstance(alpha, float):
+            raise TypeError()
+
+        for n in range(0, self.numRows):
+            for m in range(0, self.numColumns):
+                self.matrixElements[n][m] = self.matrixElements[n][m] * alpha
+
+x = Matrix(2, 3, 1, 2, 3, 4, 5, 6)
+print(x.to_string())
+x.scalar_mult(5.5)
+print(x.to_string())
